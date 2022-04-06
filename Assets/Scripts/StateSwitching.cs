5babaf4732609349c919eeae383f37e5e21942ae
Brandon Boras <brandon.boras@mymail.champlain.edu>
2022-04-06 13:49:01 -0400

    shadow switching is fully in

--- Assets/Scripts/StateSwitching.cs
+++ Assets/Scripts/StateSwitching.cs
@@ -11,22 +11,22 @@ using UnityEngine.InputSystem;
 
 public class StateSwitching : MonoBehaviour
 {
-    [Range(0, 1)] public float currentShadowAmount;
-
-    [Header("Design Vars")]
+    [Header("Variables to follow along to")]
     public bool isShadowForm;
+    [Range(0, 1)] public float currentShadowAmount;
+    [Header("Design Vars")]
     public float timeToSwitchToShadow;
     [SerializeField] Renderer playerRenderer;
 
     InputAction shadowSwapAction;
-
+    bool isWaitingToReleaseSwapButton; // allows the same button to swap between forms, but won't immediately do it
     
 
     private void Start()
     {
         isShadowForm = false;
-        
-
+        currentShadowAmount = 0;
+        isWaitingToReleaseSwapButton = false;
 
         PlayerInput pInput = this.GetComponent<PlayerInput>();
         shadowSwapAction = pInput.actions["ShadowSwap"];
@@ -42,26 +42,36 @@ public class StateSwitching : MonoBehaviour
         {
             StandardFormUpdate();
         }
+
+        if(isWaitingToReleaseSwapButton && !shadowSwapAction.IsPressed())
+        {
+            isWaitingToReleaseSwapButton = false;
+        }
     }
 
     void ShadowFormUpdate()
     {
-        if (!shadowSwapAction.IsPressed())
+        if (shadowSwapAction.IsPressed() && !isWaitingToReleaseSwapButton)
         {
-            isShadowForm = false;
+                HandleSwitching(false, false);
         }
-
-        SetAllMatShadowVal(1.0f);
+        else if (currentShadowAmount < 1)   // if they were switching, but have stopped
+        {
+            HandleSwitching(false, true);
+        }
     }
 
     void StandardFormUpdate()
     {
-        if (shadowSwapAction.IsPressed())
+        if (shadowSwapAction.IsPressed() && !isWaitingToReleaseSwapButton)
         {
-            isShadowForm = true;
-        }
 
-        SetAllMatShadowVal(0.0f);
+            HandleSwitching(true, true);
+        }
+        else if (currentShadowAmount > 0)   // if they were switching, but have stopped
+        {
+            HandleSwitching(true, false);
+        }
     }
 
     public void SetAllMatShadowVal(float newShadowAmount)
@@ -69,6 +79,37 @@ public class StateSwitching : MonoBehaviour
         foreach (Material mat in playerRenderer.materials)
         {
             mat.SetFloat("_amountConvertedToShadow", newShadowAmount);
+        }
+    }
+    public void HandleSwitching(bool isStartingFromStandardForm, bool isConvertingToShadow)
+    {
+        if (isConvertingToShadow)
+        {
+            currentShadowAmount += Time.deltaTime / timeToSwitchToShadow ;
+
+            if (currentShadowAmount > 1)
+            {
+                isShadowForm = true;
+                isWaitingToReleaseSwapButton = true;
+                currentShadowAmount = 1;
+            }
+
+            SetAllMatShadowVal(currentShadowAmount);
+        }
+        else
+        {
+            Debug.Log("Is going to standard form");
+
+            currentShadowAmount -= Time.deltaTime / timeToSwitchToShadow;
+
+            if (currentShadowAmount < 0)
+            {
+                isShadowForm = false;
+                isWaitingToReleaseSwapButton = true;
+                currentShadowAmount = 0;
+            }
+
+            SetAllMatShadowVal(currentShadowAmount);
         }
     }
 }
